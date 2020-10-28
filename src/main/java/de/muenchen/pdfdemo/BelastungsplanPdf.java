678492e31502f943dc6c097bd46360de6685a5b5
xdoo <claus.straube@gmail.com>
2020-10-28 11:39:49 +0100

    first commit

---
+++ src/main/java/de/muenchen/pdfdemo/BelastungsplanPdf.java
@@ -0,0 +1,10 @@
+package de.muenchen.pdfdemo;
+
+import lombok.Data;
+
+@Data
+public class BelastungsplanPdf {
+
+    private String title;
+    private String chart;
+}
---
+++ src/test/java/de/muenchen/pdfdemo/PdfDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.muenchen.pdfdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
